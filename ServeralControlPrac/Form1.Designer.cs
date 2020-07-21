4222b8108c165b2ab164911757ac78bd752ca6e5
LIMMIHEE <48482259+LIMMIHEE@users.noreply.github.com>
2020-07-21 14:59:26 +0900

    31-6 / 체크 박스. 체크된 박스들 내용 가져와 MessageBox 띄우기

--- ServeralControlPrac/Form1.Designer.cs
+++ ServeralControlPrac/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(224, 25);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(61, 12);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "linkLabel1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(247, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(297, 274);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- ServeralControlPrac/Form1.cs
+++ ServeralControlPrac/Form1.cs
@@ -15,6 +15,68 @@ namespace ServeralControlPrac
         public Form1()
         {
             InitializeComponent();
+            Label label = new Label();
+            label.Text = "나의 기술 블로구ㅡ";
+            label.Location = new Point(10, 10);
+            Controls.Add(label);
+
+            LinkLabel linkLabel = new LinkLabel();
+            linkLabel.Text = "https://helloworld-limmihee.tistory.com/";
+            linkLabel.Location = new Point(10, 50);
+            linkLabel.Click += LabelClick;
+            Controls.Add(linkLabel);
+
+
+            CheckBox checkBox1 = new CheckBox();
+            CheckBox checkBox2 = new CheckBox();
+            CheckBox checkBox3 = new CheckBox();
+            Button button = new Button();
+
+            checkBox1.Text = "토마토";
+            checkBox2.Text = "감자";
+            checkBox3.Text = "새우";
+            button.Text = "클릭!!";
+
+            button.Click += ButtonClick;
+
+            checkBox1.Location = new Point(20, 150);
+            checkBox2.Location = new Point(20, 190);
+            checkBox3.Location = new Point(20, 230);
+            button.Location = new Point(20, 120);
+
+            Controls.Add(checkBox1);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox3);
+            Controls.Add(button);
+
+        }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            List<String> list = new List<string>();
+
+            foreach (var item in Controls)
+            {
+                if(item is CheckBox)
+                {
+                    CheckBox checkBox = (CheckBox)item;
+                    if (checkBox.Checked)
+                    {
+                        list.Add(checkBox.Text);
+                    }
+                }
+
+            }
+
+            MessageBox.Show(String.Join(",", list));
+        }
+
+        private void LabelClick(object sender, EventArgs e)
+        {
+            System.Diagnostics
+                //.Process.Start("https://helloworld-limmihee.tistory.com/");
+                .Process.Start("notepad.exe");
+
         }
     }
 }
